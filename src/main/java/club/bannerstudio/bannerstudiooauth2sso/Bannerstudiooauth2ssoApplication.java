aad9816437289694cba93fbf5e75798c97ad440d
Ben <xxswhy109@163.com>
2022-01-12 23:15:41 +0800

    项目修改启动类

--- src/main/java/club/bannerstudio/bannerstudiooauth2sso/Bannerstudiooauth2ssoApplication.java
+++ src/main/java/club/bannerstudio/bannerstudiooauth2sso/Bannerstudiooauth2ssoApplication.java
@@ -1,10 +1,12 @@
 package club.bannerstudio.bannerstudiooauth2sso;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
-
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class Bannerstudiooauth2ssoApplication {
 
     public static void main(String[] args) {
