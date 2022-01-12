30f10962e6c4496b1a6d92bd54ad3b67a991c368
Ben <xxswhy109@163.com>
2022-01-12 23:41:10 +0800

    项目重新打包依赖

--- src/test/java/club/bannerstudio/bannerstudiooauth2sso/Bannerstudiooauth2ssoApplicationTests.java
+++ src/test/java/club/bannerstudio/bannerstudiooauth2sso/Bannerstudiooauth2ssoApplicationTests.java
@@ -15,15 +15,4 @@ class Bannerstudiooauth2ssoApplicationTests {
 
         System.out.println(new BCryptPasswordEncoder().encode("123"));
     }
-
-
-    @Test
-    public void test(){
-        String str = "78458";
-        String pattern = "^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\\d{8}$";
-        Pattern r = Pattern.compile(pattern);
-        Matcher m = r.matcher(str);
-        System.out.println(m.matches());
-    }
-
 }
