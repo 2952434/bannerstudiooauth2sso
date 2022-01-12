657b5ce264d9e20fe3e14f89858a9e2d871212e5
Ben <xxswhy109@163.com>
2022-01-13 00:34:45 +0800

    项目修改背景图片

--- src/test/java/club/bannerstudio/bannerstudiooauth2sso/Bannerstudiooauth2ssoApplicationTests.java
+++ src/test/java/club/bannerstudio/bannerstudiooauth2sso/Bannerstudiooauth2ssoApplicationTests.java
@@ -1,8 +1,10 @@
 package club.bannerstudio.bannerstudiooauth2sso;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,5 +16,12 @@ class Bannerstudiooauth2ssoApplicationTests {
     void contextLoads() {
 
         System.out.println(new BCryptPasswordEncoder().encode("123"));
+    }
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Test
+    public void test(){
+        System.out.println(passwordEncoder.encode("bannerstudioofficialwebsite"));
     }
 }
