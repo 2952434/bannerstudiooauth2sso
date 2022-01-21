866265e07272230c7756476ab263c80da2060a9d
Ben <xxswhy109@163.com>
2022-01-21 21:27:22 +0800

    debug调试

--- src/test/java/club/bannerstudio/oauth2sso/Oauth2SSOApplicationTests.java
+++ src/test/java/club/bannerstudio/oauth2sso/Oauth2SSOApplicationTests.java
@@ -1,15 +1,20 @@
 package club.bannerstudio.oauth2sso;
 
+import club.bannerstudio.oauth2sso.service.IMemberService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
 class Oauth2SSOApplicationTests {
 
+    @Autowired
+    IMemberService iMemberService;
     @Test
     void contextLoads() {
-
+        System.out.println(
+                iMemberService.selectHeadUrlByUserName("wanghaoyuan"));
     }
 
 }
